cddb8691922fb7bc0f137a977d45bb2939d19ff2
Jace <70923838+RevenantSoul@users.noreply.github.com>
2022-11-11 12:25:03 -0700

    Initial commit

---
+++ src/main/java/pyro/frost/pyrocrates/Pyrocrates.java
@@ -0,0 +1,17 @@
+package pyro.frost.pyrocrates;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public final class Pyrocrates extends JavaPlugin {
+
+    @Override
+    public void onEnable() {
+        // Plugin startup logic
+
+    }
+
+    @Override
+    public void onDisable() {
+        // Plugin shutdown logic
+    }
+}
